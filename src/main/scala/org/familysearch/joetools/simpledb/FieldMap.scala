4be493c28ddf1bd59f3b8cee1c642f54fff64897
Joe Shull <shullja@familysearch.org>
2014-05-30 16:21:47 -0600

    added ability for fieldMap's fieldList to be specified as an array.

--- src/main/scala/org/familysearch/joetools/simpledb/FieldMap.scala
+++ src/main/scala/org/familysearch/joetools/simpledb/FieldMap.scala
@@ -3,5 +3,9 @@ package org.familysearch.joetools.simpledb
 
 trait FieldMap[T] {
   def get(instance: T, fieldName: String):AnyRef = null
-  def fieldNames: List[String] = List()
+  def fieldNames: List[String] = fieldNamesArray.toList
+
+  protected def fieldNamesArray: Array[String] = {
+    Array[String]()
+  }
 }
