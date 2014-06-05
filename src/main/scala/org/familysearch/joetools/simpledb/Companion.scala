7a6a0e70b6c19c3f01486964e3ed3ddefc39ff08
Joe Shull <shullja@familysearch.org>
2014-06-05 17:58:30 -0600

    removed 'abstract' from companion

--- src/main/scala/org/familysearch/joetools/simpledb/Companion.scala
+++ src/main/scala/org/familysearch/joetools/simpledb/Companion.scala
@@ -3,7 +3,7 @@ package org.familysearch.joetools.simpledb
 import java.lang.reflect.Field
 import scala.reflect.Manifest
 
-abstract class Companion[T](clazz: Class[_]) {
+class Companion[T](clazz: Class[_]) {
   def this()(implicit m: Manifest[T])=this(m.erasure)
   private def getDeclaredFields = clazz.getDeclaredFields
   def fieldAndInstanceToFieldNameAndValueMap(instance: T): (Field)=>(String, AnyRef) = {(f: Field) => {f.setAccessible(true); (f.getName, f.get(instance))}}
